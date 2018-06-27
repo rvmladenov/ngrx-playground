c4a97780f4670045c891a2087169c9077a070819
Radoslav Mladenov <rmladenov@soft2run.com>
2018-06-27 14:18:47 +0300

    added ngx-store-freeze

--- auth/src/app/reducers/index.ts
+++ auth/src/app/reducers/index.ts
@@ -6,6 +6,7 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import { storeFreeze } from 'ngrx-store-freeze';
 
 export interface AppState {
 
@@ -16,4 +17,4 @@ export const reducers: ActionReducerMap<AppState> = {
 };
 
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [storeFreeze] : [];
