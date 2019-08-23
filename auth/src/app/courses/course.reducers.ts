e490044909799b0559c7e69c6ec75e8cc11ae2c1
Radoslav <rvmladenov@gmail.com>
2019-08-23 14:44:37 +0300

    last changes

--- auth/src/app/courses/course.reducers.ts
+++ auth/src/app/courses/course.reducers.ts
@@ -1,9 +1,16 @@
 import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
 import { Course } from './model/course';
 
-
 export interface CoursesState extends EntityState<Course> {
 
 }
 
 export const adapter: EntityAdapter<Course> = createEntityAdapter<Course>();
+
+
+export const initialState: State = featureAdapter.getInitialState(
+    {
+      isLoading: false,
+      error: null
+    }
+  );
